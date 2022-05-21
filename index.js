cf7e6a4a4237107386528c83af0d2a295c6736d0
Fabio Viscuso <viscuso.fabio@outlook.it>
2022-05-21 19:07:18 +0200

    feat: rebrande to be a boilerplate

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 require("dotenv").config({ path: "./.env" });
-const fs = require("fs");
 
 /* Within app we call the top-level function exported by express module */
 const app = express();
@@ -33,94 +32,95 @@ mongoose
 
 /* Define a Schema, the actual structure that holds your data */
 const contactRequestSchema = new mongoose.Schema({
-    firstName: String,
-    lastName: String,
-    otherNames: Array,
-    phone: Number,
-    email: String,
-    contactMethod: String,
-    job: String,
-    hasAgreedTerms: Boolean,
+    firstName: { type: String, required: true, trim: true, maxlength: 20 },
+    lastName: { type: String, required: true, trim: true, maxlength: 20 },
+    age: {
+        type: Number,
+        required: true,
+        min: [18, "Age must be > 18"],
+        max: [150, "Age is too high"],
+    },
+    phone: { type: String, required: true, trim: true, maxlength: 16 },
+    email: { type: String, required: true, trim: true, maxlength: 40 },
+    contactMethod: {
+        type: String,
+        required: true,
+        enum: ["job-apply", "mail-info", "call-info"],
+    },
+    userJob: {
+        type: String,
+        required: true,
+        enum: ["developer", "artist", "management"],
+    },
+    message: {
+        type: String,
+        required: false,
+        default: "no-message",
+        trim: true,
+    },
+    hasAgreedTerms: { type: Boolean, required: true },
+    hasBeenContacted: { type: Boolean, required: false, default: false },
 });
+
 /* Define a Model, it will pluralized and de-capitalized automatically inside mongo server */
 const ContactRequest = mongoose.model("ContactRequest", contactRequestSchema);
-/* Create a POST route */
-app.post("/newContact", (req, res) => {
-    /* Save the request body to a constant */
-    const incomingObj = req.body;
-    /* Create an object based on the model */
-    const contactEntry = new ContactRequest({ ...incomingObj });
-    console.log(contactEntry);
-    /* Save in remote DB */
-    contactEntry.save();
-    /* Send back the object */
-    res.send(contactEntry);
-});
+
+/* A mongoose "virtual" is, indeed, a virtual property that doesn't really
+   exist in the schema, but it's read by a "shadow" getter and written by a
+   "shadow" setter. Useful not in DB, but in the mongoose/JS realm */
+contactRequestSchema
+    .virtual("fullName")
+    .get(function () {
+        return `${this.firstName} ${this.lastName}`;
+    })
+    .set(function (name) {
+        [this.firstName, this.lastName] = name.split(" ");
+    });
+
+/* Schemas can have their own static classes, not available to instances */
+contactRequestSchema.statics.updateToNewSchema = function () {
+    console.log("placeholder");
+};
+
+/* We can define custom methods in our schemas */
+contactRequestSchema.methods.contactToggle = function () {
+    this.hasBeenContacted = !this.hasBeenContacted;
+    /* This returns a promise-like object, then-able in the calling function */
+    return this.save();
+};
 
 /* Set up the home route via GET */
 app.get("/", (req, res) => res.send("Welcome to the backend server!"));
 
 /* CRUD ENDPOINTS ---------------------------------*/
 
-/* Save path to .json file as a constant */
-const jsonPath = __dirname + "/data.json";
-/* Create an array as temporary DB and initialize it with
-   the contents of data.json */
-function initializeDB(path) {
-    console.log(path);
-    return fs.readFileSync(path).toString("utf-8");
-}
-
-const basicDB = JSON.parse(initializeDB(jsonPath));
-
-console.log(basicDB);
-
-/* Set POST route */
-app.post("/data", (req, res) => {
-    console.log(req.body);
-    basicDB.push(req.body);
-    console.log(basicDB);
-
-    fs.truncate(jsonPath, 0, () => {
-        fs.writeFile(jsonPath, JSON.stringify(basicDB), (err) => {
-            if (err) {
-                return console.log("Error writing file: " + err);
-            } else {
-                res.status(200).json({
-                    message: "File successfully written",
-                });
-            }
-        });
-    });
+/* Get contact(s) */
+app.post("/get-contacts", async (req, res) => {
+    const contactsFound = await ContactRequest.findOne({ ...req.body });
+    res.json(contactsFound);
 });
 
-/* Set GET route */
-app.get("/data", (req, res) => {
-    res.header("Content-Type", "application/json");
-    fs.readFile(jsonPath, (err, data) => {
-        res.send(data);
-    });
+/* Updated contact(s) */
+app.patch("/update-contacts", (req, res) => {
+    res.send({ message: "placeholder for PATCH route" });
 });
 
-/* Set DELETE route */
-app.delete("/data", (req, res) => {
-    console.log(req.body);
-    if (req.body.key === "panopticon") {
-        basicDB.length = 0;
-        fs.writeFile(jsonPath, JSON.stringify(basicDB), (err) => {
-            if (err) {
-                console.log("Error: ", err);
-            } else {
-                res.status(200).json({
-                    message: "Data successfully erased!",
-                });
-            }
-        });
-    } else {
-        res.status(401).json({
-            message: "Access denied: invalid key",
-        });
-    }
+/* Delete contact(s) */
+app.delete("/delete-contacts", (req, res) => {
+    res.send({ message: "placeholder for DELETE route" });
+});
+
+/* Create new contact */
+app.post("/new-contact", async (req, res) => {
+    /* Create an object based on the model with data taken from the request */
+    const contactEntry = await new ContactRequest({ ...req.body });
+    console.log(contactEntry);
+    /* Save in remote DB */
+    contactEntry
+        .save()
+        /* Send back the object for logging */
+        .then((data) => res.status(200).json({ "new saved entry: ": data }))
+        .catch((err) => console.log(err));
 });
 
 /* Launch the server on specified PORT and print a log */
